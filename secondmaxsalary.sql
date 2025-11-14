6f06b32d312cd0620f1953510cdaaf72b5726bd6
amitt18 <amitrtiwari500@gmail.com>
2025-11-14 12:53:33 +0530

    Query for finding second Highest Salary

--- secondmaxsalary.sql
+++ secondmaxsalary.sql
@@ -1,8 +1,18 @@
-select distinct esal as SecondMaxSalary
-from employee 
-order by esal desc 
-limit 1 offset 1
+-- -------------------------------------------------------
+-- File: secondmaxsalary.sql
+-- Description: Query to find the employee(s) with the 
+--              second highest salary.
 
+SELECT ename, esal
+FROM employee
+WHERE esal = (
+    SELECT DISTINCT esal
+    FROM employee
+    ORDER BY esal DESC
+    LIMIT 1 OFFSET 1
+);
 
-
-
+-- -------------------------------------------------------
+-- Output of the Query
+  SecondMaxSalary
+   55000
